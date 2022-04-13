08199ac68d3586072e7a22f732eb9234ca7911d8
talentwill <talentwill@outlook.com>
2022-04-13 23:34:50 +0800

    减绳子

---
+++ cpp/LeetCode/202204/0413.cpp
@@ -0,0 +1,49 @@
+#include "../helper.h"
+
+// 减绳子
+
+class Solution1
+{
+public:
+    int cuttingRope(int n) {
+        vector<int> products = { 0, 1, 2, 3 };
+        products.resize(n + 1);
+        int ans = 0;
+        for (int i = 4; i <= n; i++) {
+            for (int j = 1; j <= i / 2; j++) {
+                ans = max(ans, products[j] * products[i - j]);
+            }
+            products[i] = ans;
+        }
+        return products[n];
+    }
+};
+
+class Solution2 {
+public:
+    int cuttingRope(int n) {
+        if (n <= 3)
+            return n - 1;
+
+        int quotient = n / 3;
+        int remainder = n % 3;
+
+        if (remainder == 0) { // 整除
+            remainder = 1;
+        }
+        else if (remainder == 1) { // 余1，把 3+1 变成 2+2
+            remainder = 4;
+            quotient--; //
+        }
+        long res = remainder;
+        for (int i = 0; i < quotient; i++) {
+            res = (res * 3) % 1000000007;
+        }
+        return res;
+    }
+};
+
+int main() {
+    cout << Solution2().cuttingRope(10);
+    return 0;
+}
--- cpp/LeetCode/helper.h
+++ cpp/LeetCode/helper.h
@@ -11,6 +11,7 @@
 #include <set>
 
 #include <string>
+#include <cmath>
 
 using namespace std;
 
