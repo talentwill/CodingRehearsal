31d68ffa421b475098e00650deb6492dbf9d8122
talentwill <talentwill@outlook.com>
2022-08-20 02:17:30 +0800

    Complete exercise to implement generic type

--- rust/Take-your-first-steps-with-Rust/demo/rust_generic_types_traits/src/u06_exercise_implement_generic_type.rs
+++ rust/Take-your-first-steps-with-Rust/demo/rust_generic_types_traits/src/u06_exercise_implement_generic_type.rs
@@ -1,21 +1,26 @@
 //https://docs.microsoft.com/en-us/learn/modules/rust-generic-types-traits/6-exercise-implement-generic-type
 
-struct Container {
-    value: u32,
+struct Container<T> {
+    value: T,
 }
 
-impl Container {
-    pub fn new(value: u32) -> Self {
+// https://doc.rust-lang.org/stable/book/ch10-01-syntax.html#in-method-definitions
+// however, if is weird to add T twice.
+impl<T> Container<T> {
+    pub fn new(value: T) -> Self {
         Container { value }
     }
 }
 
 pub fn test() {
     assert_eq!(Container::new(42).value, 42);
-    // assert_eq!(Container::new(3.14).value, 3.14);
-    // assert_eq!(Container::new("Foo").value, "Foo");
-    // assert_eq!(Container::new(String::from("Bar")).value, String::from("Bar"));
-    // assert_eq!(Container::new(true).value, true);
-    // assert_eq!(Container::new(-12).value, -12);
-    // assert_eq!(Container::new(Some("text")).value, Some("text"));
+    assert_eq!(Container::new(3.14).value, 3.14);
+    assert_eq!(Container::new("Foo").value, "Foo");
+    assert_eq!(
+        Container::new(String::from("Bar")).value,
+        String::from("Bar")
+    );
+    assert_eq!(Container::new(true).value, true);
+    assert_eq!(Container::new(-12).value, -12);
+    assert_eq!(Container::new(Some("text")).value, Some("text"));
 }
