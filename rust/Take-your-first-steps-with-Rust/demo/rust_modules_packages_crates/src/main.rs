3a613a77a5d49c41664d619085514f81c6359c97
talentwill <talentwill@outlook.com>
2022-08-20 15:22:52 +0800

    [rust] add Regex demo

--- rust/Take-your-first-steps-with-Rust/demo/rust_modules_packages_crates/src/main.rs
+++ rust/Take-your-first-steps-with-Rust/demo/rust_modules_packages_crates/src/main.rs
@@ -1,7 +1,11 @@
 mod authentication;
+use regex::Regex;
 
 fn main() {
     let mut user = authentication::User::new("Hello World", "123456");
     println!("The username is: {}", user.get_username());
     user.set_password("654321");
+
+    let re = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
+    println!("Did our date match? {}", re.is_match("2022-08-20"));
 }
