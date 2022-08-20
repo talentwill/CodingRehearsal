dd2127611e9b6e6bf5da33c27e966cc29b72721c
talentwill <talentwill@outlook.com>
2022-08-21 01:08:12 +0800

    [rust] complete tasks's functions

--- rust/Take-your-first-steps-with-Rust/rusty-journal/src/task.rs
+++ rust/Take-your-first-steps-with-Rust/rusty-journal/src/task.rs
@@ -1,4 +1,13 @@
-use chrono::{serde::ts_seconds, DateTime, Utc};
+// https://docs.microsoft.com/en-us/learn/modules/rust-create-command-line-program/7-list-tasks-function
+
+use std::{
+    fmt,
+    fs::{File, OpenOptions},
+    io::{Error, ErrorKind, Result, Seek, SeekFrom},
+    path::PathBuf,
+};
+
+use chrono::{serde::ts_seconds, DateTime, Local, Utc};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -16,4 +25,67 @@ impl Task {
             created_at: Utc::now(),
         }
     }
+}
+
+impl fmt::Display for Task {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let create_at = self.created_at.with_timezone(&Local).format("%F %H:%M");
+        write!(f, "{:<50} [{}]", self.text, create_at)
+    }
+}
+
+fn collect_tasks(mut file: &File) -> Result<Vec<Task>> {
+    file.seek(SeekFrom::Start(0))?;
+    let tasks: Vec<Task> = match serde_json::from_reader(file) {
+        Ok(tasks) => tasks,
+        Err(e) if e.is_eof() => Vec::new(),
+        Err(e) => Err(e)?,
+    };
+    file.seek(SeekFrom::Start(0))?;
+    Ok(tasks)
+}
+
+pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
+    let file = OpenOptions::new()
+        .write(true)
+        .read(true)
+        .create(true)
+        .open(journal_path)?;
+
+    let mut tasks = collect_tasks(&file)?;
+
+    tasks.push(task);
+    serde_json::to_writer(&file, &tasks)?;
+    Ok(())
+}
+
+pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<()> {
+    let file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .open(journal_path)?;
+
+    let mut tasks = collect_tasks(&file)?;
+    if task_position == 0 || task_position > tasks.len() {
+        return Err(Error::new(ErrorKind::InvalidInput, "Invalid Task ID"));
+    }
+
+    tasks.remove(task_position - 1);
+    file.set_len(0)?;
+    serde_json::to_writer(&file, &tasks)?;
+    Ok(())
+}
+
+pub fn list_tasks(journal_path: PathBuf) -> Result<()> {
+    let file = OpenOptions::new().read(true).open(journal_path)?;
+    let tasks = collect_tasks(&file)?;
+    if tasks.is_empty() {
+        println!("The list is empty.");
+    } else {
+        let order: u32 = 1;
+        for task in tasks {
+            println!("{}: {}", order, task);
+        }
+    }
+    Ok(())
 }
