7ec0e86407f5578509fd5e0f545b8f7ac6d7460a
talentwill <talentwill@outlook.com>
2022-08-21 01:42:46 +0800

    [rust] combine task and commandlineargs, so that we can have it a try now.

--- rust/Take-your-first-steps-with-Rust/rusty-journal/src/main.rs
+++ rust/Take-your-first-steps-with-Rust/rusty-journal/src/main.rs
@@ -1,8 +1,25 @@
 use structopt::StructOpt;
+use crate::cli::Action::*;
+use crate::cli::CommandLineArgs;
+use crate::task::Task;
 
 mod cli;
 mod task;
 
 fn main() {
-    println!("{:#?}", cli::CommandLineArgs::from_args());
+    // Destructure, the fields can be used independently
+    let CommandLineArgs {
+        action,
+        journal_file,
+    } = CommandLineArgs::from_args();
+
+    // Unpack the journal file
+    let journal_file = journal_file.expect("Failed to find a journal file");
+
+    // Perform the actions
+    match action {
+        Add{text} => task::add_task(journal_file, Task::new(text)),
+        Done {position} => task::complete_task(journal_file, position),
+        List => task::list_tasks(journal_file),
+    }.expect("Failed to perform action")
 }
